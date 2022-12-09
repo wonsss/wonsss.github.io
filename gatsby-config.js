12e971fbed6292b97ac80ff1a0f04f05fd60d0c5
wonsss <bigdatajang@gmail.com>
2022-12-10 02:56:30 +0900

    feat: gatsby 구글 애널리틱스 적용
    https://devfoxstar.github.io/web/gatsby-ga-tracking/

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,124 +1,124 @@
 const metaConfig = require('./gatsby-meta-config')
 
 module.exports = {
-    siteMetadata: metaConfig,
-    plugins: [
-        {
-            resolve: `gatsby-source-filesystem`,
+  siteMetadata: metaConfig,
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/__about`,
+        name: `about`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-katex`,
             options: {
-                path: `${__dirname}/content/blog`,
-                name: `blog`,
+              strict: `ignore`,
             },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
+          },
+          {
+            resolve: `gatsby-remark-images`,
             options: {
-                path: `${__dirname}/content/__about`,
-                name: `about`,
+              maxWidth: 1200,
+              linkImagesToOriginal: false,
             },
-        },
-        {
-            resolve: `gatsby-source-filesystem`,
+          },
+          {
+            resolve: `gatsby-remark-images-medium-zoom`,
             options: {
-                path: `${__dirname}/content/assets`,
-                name: `assets`,
+              margin: 36,
+              scrollOffset: 0,
             },
-        },
-        {
-            resolve: `gatsby-transformer-remark`,
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
             options: {
-                plugins: [
-                    {
-                        resolve: `gatsby-remark-katex`,
-                        options: {
-                            strict: `ignore`,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-images`,
-                        options: {
-                            maxWidth: 1200,
-                            linkImagesToOriginal: false,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-images-medium-zoom`,
-                        options: {
-                            margin: 36,
-                            scrollOffset: 0,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-responsive-iframe`,
-                        options: {
-                            wrapperStyle: `margin-bottom: 1.0725rem`,
-                        },
-                    },
-                    {
-                        resolve: `gatsby-remark-prismjs`,
-                        options: {
-                            inlineCodeMarker: '%',
-                        },
-                    },
-                    `gatsby-remark-copy-linked-files`,
-                    `gatsby-remark-smartypants`,
-                    `gatsby-remark-autolink-headers`,
-                    `gatsby-remark-emoji`,
-                ],
+              wrapperStyle: `margin-bottom: 1.0725rem`,
             },
-        },
-        {
-            resolve: `gatsby-plugin-google-analytics`,
+          },
+          {
+            resolve: `gatsby-remark-prismjs`,
             options: {
-                trackingId: metaConfig.ga,
-                head: true,
-                anonymize: true,
+              inlineCodeMarker: '%',
             },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: metaConfig.title,
-                short_name: metaConfig.title,
-                start_url: `/`,
-                background_color: `#ffffff`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: metaConfig.icon,
-            },
-        },
-        {
-            resolve: `gatsby-plugin-typography`,
-            options: {
-                pathToConfigModule: `src/utils/typography`,
-            },
-        },
-        {
-            resolve: 'gatsby-plugin-robots-txt',
-            options: {
-                host: 'https://wonsss.github.io',
-                sitemap: 'https://wonsss.github.io/sitemap.xml',
-                policy: [
-                    {
-                        userAgent: '*',
-                        allow: '/',
-                    },
-                ],
-            },
-        },
-        {
-            resolve: `gatsby-plugin-google-adsense`,
-            options: {
-                publisherId: metaConfig.ad,
-            },
-        },
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
-        `gatsby-plugin-feed`,
-        `gatsby-plugin-offline`,
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-lodash`,
-        `gatsby-plugin-sitemap`,
-    ],
+          },
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-emoji`,
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: metaConfig.ga,
+        head: true,
+        anonymize: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: metaConfig.title,
+        short_name: metaConfig.title,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: metaConfig.icon,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://wonsss.github.io',
+        sitemap: 'https://wonsss.github.io/sitemap.xml',
+        policy: [
+          {
+            userAgent: '*',
+            allow: '/',
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-adsense`,
+      options: {
+        publisherId: metaConfig.ad,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-feed`,
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-lodash`,
+    `gatsby-plugin-sitemap`,
+  ],
 }
--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -1,33 +1,33 @@
 module.exports = {
-    title: `Marco`,
-    description: `Frontend 기술 블로그`,
-    author: `Marco`,
-    introduction: ``,
-    siteUrl: `https://wonsss.github.io`, // Your blog site url
-    social: {
-        twitter: ``, // Your Twitter account
-        github: `wonsss`, // Your GitHub account
-        medium: ``, // Your Medium account
-        facebook: ``, // Your Facebook account
-        linkedin: ``, // Your LinkedIn account
-        instagram: ``, // Your Instagram account
-        velog: `jangws`,
-    },
-    icon: `content/assets/logo.png`, // Add your favicon
-    keywords: [`blog`, `javascript`, `frontend`, `typescript`, `react`],
-    comment: {
-        disqusShortName: '', // Your disqus-short-name. "marco-tech" not using disqus
-        utterances: 'wonsss/wonsss.github.io', // Your repository for archive comment
-    },
-    configs: {
-        countOfInitialPost: 10, // Config your initial count of post
-    },
-    sponsor: {
-        buyMeACoffeeId: 'marcotech',
-    },
-    share: {
-        facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
-    },
-    ga: 'G-S31WXL68LE', // Add your google analytics tranking ID
-    ad: '186-692-8085', // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
+  title: `Marco`,
+  description: `Frontend 기술 블로그`,
+  author: `Marco`,
+  introduction: ``,
+  siteUrl: `https://wonsss.github.io`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `wonsss`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+    linkedin: ``, // Your LinkedIn account
+    instagram: ``, // Your Instagram account
+    velog: `jangws`,
+  },
+  icon: `content/assets/logo.png`, // Add your favicon
+  keywords: [`blog`, `javascript`, `frontend`, `typescript`, `react`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. "marco-tech" not using disqus
+    utterances: 'wonsss/wonsss.github.io', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'marcotech',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: `G-S31WXL68LE`, // Add your google analytics tranking ID
+  ad: `186-692-8085`, // Add your google adsense publisherId `ca-pub-xxxxxxxxxx`
 }
